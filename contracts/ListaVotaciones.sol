77bca58c97a78cb8231050f835c6f2befbfee6c0
Fernando Ruiz <32452140+FenriRuiz@users.noreply.github.com>
2021-01-03 19:16:17 +0100

    Funcion agregarCandidato implementada
    BackEnd: Función para agregar candidatos a una votación implementado.
    Falta añadir que se pueda modificar solo cuando el estado de la votación se encuentre en edición y posteriormente testear el funcionamiento e implementar el cliente.

--- contracts/ListaVotaciones.sol
+++ contracts/ListaVotaciones.sol
@@ -5,39 +5,61 @@ contract ListaVotaciones{
     Estado constant estadoBase = Estado.Editando;
     uint public numVotaciones = 0;
 
-    // struct Votante {
-    //     uint id_votante;
-    //     string candidato_elegido;
-    //     bool ha_votado;
-    // }
+    struct Votante {
+        string nombre;
+        string candidato_elegido;
+        bool ha_votado;
+    }
 
     struct Votacion {
         uint id_votacion;
         string titulo;
         address id_creador;
         Estado estado;
-        mapping (uint => string) candidatos;
-        // mapping (uint => Votante) votantes;
+        string[] candidatos;
+        mapping(string => Votante) votantes;
     }
 
     mapping (uint => Votacion) public votaciones;
 
     constructor() public {
         nuevaVotacion("Votación a delegado de centro");
+
     }
+
+
 
     function nuevaVotacion(string memory _titulo) public{
         //Crear una nueva votación y añadirla a la lista de votaciones
         //Emitir la nueva votación
-
+        string[] memory _candidatos;
         numVotaciones ++;
-        votaciones[numVotaciones] = Votacion(numVotaciones, _titulo, msg.sender, estadoBase);
+        votaciones[numVotaciones] = Votacion(numVotaciones, _titulo, msg.sender, estadoBase, _candidatos);
     }
-    
-    function agregarCandidato() public{
+
+    function agregarCandidato(uint numVotacion, string memory candidato) public {
         //Si es el creador de la votación
-        //Si no existe el nuevo candidato en la votacion
-        //Añade el candidato a la votacion.
+        require(
+            msg.sender == votaciones[numVotacion].id_creador,
+            "Solo el creador de la votación puede llamar a esta función"
+        );
+        // Si no hay ningún candidato en la votación se añade
+        if(votaciones[numVotacion].candidatos.length==0){
+            votaciones[numVotacion].candidatos.push(candidato);
+        }
+        // Si hay alguno se comprueba que el nuevo no exista ya.
+        else{
+            for (uint i = 0; i < votaciones[numVotacion].candidatos.length; i++){
+                // Comparación de string en soldity @-@
+                if(keccak256(abi.encodePacked(votaciones[numVotacion].candidatos[i]))==keccak256(abi.encodePacked(candidato))){
+                    revert("El candidato se encuentra en la votación");
+                }
+            }        
+            //Si no existe el nuevo candidato en la votacion
+            //Añade el candidato a la votacion.
+            votaciones[numVotacion].candidatos.push(candidato);
+        }
+
     }
 
     function accederVotacion() public{
